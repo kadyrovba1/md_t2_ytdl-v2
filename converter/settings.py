49fd53b5262f8be9a76d51e4cf299db6239ed04b
Beksultan Kadyrov <scarpion.kg@gmail.com>
2019-06-11 03:46:20 +0300

    email settings

--- converter/settings.py
+++ converter/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .secretkeys import mail_pass
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,4 +126,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'scarpion.kg@gmial.com'
-EMAIL_HOST_PASSWORD = 'pochemutakaya'
+EMAIL_HOST_PASSWORD = mail_pass
